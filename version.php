1fdcbd3547bb1e7d7ed7330e41390337a6de8e49
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-08-29 17:49:28 -0300

    chore(git): [DEL] Remove arquivos do .idea/ do versionamento
    Motivo: arquivos de configuração do IDE (JetBrains) não deveriam ser
    versionados, pois são específicos do ambiente local de cada desenvolvedor.
    
    Impacto: os arquivos deixam de ser rastreados pelo git (permanecem no
    disco de quem já os tinha) e futuras alterações neles não serão mais
    commitadas.
    
    Arquivos/Mudanças:
    - .gitignore: adiciona a entrada .idea/
    - .idea/auth_suap.iml, .idea/inspectionProfiles/profiles_settings.xml,
      .idea/modules.xml: removidos do índice do git (git rm --cached)
    
    Refs #45

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'auth_suap';
-$plugin->release = '4.5.079';
-$plugin->version = 2026_08_29_079;
+$plugin->release = '4.5.080';
+$plugin->version = 2026_08_29_080;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2024100700;
